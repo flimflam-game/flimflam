703bc47384311334a6c292200f72f8f96fcf9d0c
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-26 22:03:05 +1100

    Handle each stream in a separate thread

--- crates/flimflam-server/src/main.rs
+++ crates/flimflam-server/src/main.rs
@@ -1,25 +1,31 @@
 use flimflam_model::Event;
+use parking_lot::Mutex;
 use std::io::BufReader;
 use std::net::TcpListener;
+use std::sync::Arc;
+use std::thread;
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:1234")?;
-    let mut clients = Vec::new();
+    let clients = Arc::new(Mutex::new(Vec::new()));
 
     for stream in listener.incoming() {
         let mut stream = BufReader::new(stream?);
+        let clients = Arc::clone(&clients);
 
-        loop {
-            let event = jsonl::read(&mut stream)?;
-            dbg!(&event);
+        thread::spawn(move || -> anyhow::Result<()> {
+            loop {
+                let event = jsonl::read(&mut stream)?;
+                dbg!(&event);
 
-            match event {
-                Event::JoinGame(client) => clients.push(client),
-                Event::PlayerMoved(_) => {}
+                match event {
+                    Event::JoinGame(client) => clients.lock().push(client),
+                    Event::PlayerMoved(_) => {}
+                }
+
+                dbg!(&clients);
             }
-
-            dbg!(&clients);
-        }
+        });
     }
 
     Ok(())
